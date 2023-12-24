53ac4fadddaa121d15b629cfaaf7bc034ba11668
Hassan Karrach <hkarrach@c3r10p4.1337.ma>
2023-12-24 11:53:33 +0100

    few changes

--- gnl2/get_next_line.c
+++ gnl2/get_next_line.c
@@ -1,12 +1,5 @@
 #include "get_next_line.h"
 
-// typedef struct t_list{
-// 	char *Line;
-// 	struct t_list *Next;
-// }
-
-
-
 int check_the_end_of_line(char *buff)
 {
 	int i;
@@ -26,20 +19,60 @@ char	*extract_line(char *str)
 {
 	char *final_line;
 	int line_len;
+	int i;
 
 	line_len = 0;
-	while(str[line_len] != '\n' && str[line_len])
-	{
+	i = 0;
+	while(str[line_len] != '\n' && str[line_len]) //get \n index.
 		line_len++;
-	}
 
-	printf("%d\n", line_len);
-	return (NULL);
+	line_len++; //add \n also.
+	final_line = malloc(sizeof(char) * line_len);
+	while(str[line_len] && line_len)
+	{
+		final_line[i] = str[i];
+		line_len--;
+		i++;
+	}
+	final_line[i] = '\0';
+	return (final_line);
 }
 
-char	*get_exact_line(int fd,char *buff)
+char	*get_rest_of_line(char *str)
 {
-	char *line;
+	char *final_line;
+	int line_len;
+	int full_len;
+	int i;
+
+	line_len = 0;
+	full_len = 0;
+	i = 0;
+	while(str[line_len] != '\n' && str[line_len])
+		line_len++;
+	while(str[full_len])
+		full_len++;
+	final_line = malloc(sizeof(char) * (full_len - line_len));
+	line_len++; //Skip \n
+
+	while(str[line_len])
+	{
+		final_line[i] = str[line_len];
+		i++;
+		line_len++;
+	}
+	final_line[i]= '\0';
+	return (final_line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char *line;
+	char *buff;
+
+	buff = malloc(sizeof(char) * 10);
+	if(!buff)
+		return (0);
 
 	line = NULL;
 	read(fd, buff, 9);
@@ -50,22 +83,18 @@ char	*get_exact_line(int fd,char *buff)
 		read(fd, buff, 9);
 		line = ft_strjoin(line, buff);
 	}
-	return (line);
+	return (extract_line(line));
 }
 
 int main()
 {
 	int fd;
 	int bytesReaded;
-	char *buff;
 
 	fd = open("text.txt", O_RDONLY);
 	if (fd == -1)
 		printf("error!");
 
-	buff = malloc(sizeof(char) * 10);
-	if(!buff)
-		return (0);
-
-	extract_line(get_exact_line(fd, buff));
+	printf("\n-%s", get_next_line(fd));
+	// printf("\n-%s", get_next_line(fd));
 }
