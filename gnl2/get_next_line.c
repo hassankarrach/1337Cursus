91439b2ed6e3bc12c2bb9d64b9183d0b080e1e6e
hassankarrach <hassan.winners@gmail.com>
2023-12-26 05:20:33 +0100

    test

--- gnl2/get_next_line.c
+++ gnl2/get_next_line.c
@@ -1,81 +1,123 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkarrach <hkarrach@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/25 22:03:36 by hkarrach          #+#    #+#             */
-/*   Updated: 2023/12/26 01:08:00 by hkarrach         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 
-void	free_it(char *str)
+char	*get_the_line(char *str)
 {
-	if (str)
-		free(str);
-	str = NULL;
-}
+	int i; //len until '\n'
+	char *res;
 
-int	ft_strchr( char *s, int readit)
-{
-	int	i;
-
-	i = 1;
-	while (s[i - 1] || readit >= i)
+	if(!str)
+		return (NULL);
+	i = 0;
+	while(str[i] != '\n')
+		i++;
+	res = malloc(sizeof(char) * (i + 1));
+	i = 0;
+	while (str[i] && str[i] != '\n')
 	{
-		if (s[i - 1] == '\n')
-			return (i);
+		res[i] = str[i];
 		i++;
 	}
-	return (0);
+	res[i] = '\0';
+	return (res);
 }
 
-char	*read_and_getline(char *buf, int fd, char *line)
+char	*get_the_rest(char *str)
 {
-	int	readit;
-	int	endl;
+	int		i;
+	int		j;
+	int		len;
+	char	*res;
 
-	readit = 1;
-	endl = 0;
-	while (endl == 0 && readit > 0)
+	i = 0;
+	j = 0;
+	len = ft_strlen(str);
+	if(!str)
+		return (NULL);
+	while (str[i] && str[i] != '\n')
+		i++;
+	if(!str[i])
 	{
-		readit = read(fd, buf, BUFFER_SIZE);
-		buf[readit] = '\0';
-		if (readit == 0)
-			return (shift(buf, BUFFER_SIZE), line);
-		endl = ft_strchr(buf, readit);
-		line = ft_strjoin(line, buf);
-		if (!line)
+		free(str);
+		return (NULL);
+	}
+
+	res = malloc(sizeof(char) * (len - i));
+	if (!res)
+		return (NULL);
+
+	i++; //Skip '\n'
+	while (str[i])
+	{
+		res[j] = str[i];
+		i++;
+		j++;
+	}
+	res[j] = '\0';
+
+	free(str);
+	return (res);
+}
+
+char	*read_line(int fd, char *str)
+{
+	char	*buff;
+	int		bytes_readed;
+
+	bytes_readed = 1;
+	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	while (bytes_readed && !ft_strchr(str, '\n'))
+	{
+		bytes_readed = read(fd, buff, BUFFER_SIZE);
+		if(bytes_readed < 0)
+		{
+			free(str);
+			free(buff);
 			return (NULL);
-		if (endl)
-			shift(buf, endl);
+		}
+		buff[bytes_readed] = '\0';
+		str = ft_strjoin(str, buff);
+	}
+	free(buff);
+	return (str);
+}
+
+
+char	*get_next_line(int fd)
+{
+	static char *str;
+	char 		*line;
+
+	if(fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	str = read_line(fd, str);
+	if(!str)
+		return (NULL);
+	line = get_the_line(str);
+	str  = get_the_rest(str);
+	if(line[0] == '\0')
+	{
+		free(line);
+		return (NULL);
 	}
 	return (line);
 }
 
-char	*get_next_line(int fd)
+int main()
 {
-	static char	buf[BUFFER_SIZE + 1];
-	char		*line;
-	int			endl;
+    int fd = open("text.txt", O_RDONLY);  
+    char *line;
 
-	line = NULL;
-	endl = 0;
-	if ((read(fd, buf, 0) == -1) || (OPEN_MAX < fd || fd < 0))
-		return (shift(buf, BUFFER_SIZE), NULL);
-	if (*buf != 0)
-	{
-		line = ft_strjoin(line, buf);
-		if (!line)
-			return (NULL);
-		endl = ft_strchr(buf, BUFFER_SIZE);
-		if (!endl)
-			shift(buf, BUFFER_SIZE);
-		else
-			return (shift(buf, endl), line);
-	}
-	return (read_and_getline(buf, fd, line));
+    if (fd == -1)
+    {
+        perror("Error opening file");
+        return 1;
+    }
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("Line: %s\n", line);
+        free(line);
+    }
+
+    close(fd); 
+    return 0;
 }
