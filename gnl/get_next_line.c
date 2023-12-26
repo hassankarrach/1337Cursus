f02b19da29939282879f654e2aa3787c360de7d7
hassankarrach <hassan.winners@gmail.com>
2023-12-26 01:32:09 +0100

    testing

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -1,120 +1,40 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkarrach <hkarrach@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/05 17:41:52 by hkarrach          #+#    #+#             */
-/*   Updated: 2023/12/20 03:17:19 by hkarrach         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 
-char	*get_line(char *str)
+char	*ft_read_to_left_str(int fd, char *left_str)
 {
-	char	*line;
-	int		i;
-	int		j;
+	char	*buff;
+	int		rd_bytes;
 
-	j = 0;
-	i = 0;
-	while (str[i] && str[i] != '\n')
-		i++;
-	if (str[i] == '\n')
-		line = malloc((sizeof(char) * i) + 2);
-	else
-		line = malloc(sizeof(char) * i + 1);
-	while (j < i)
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	rd_bytes = 1;
+	while (!ft_strchr(left_str, '\n') && rd_bytes != 0)
 	{
-		line[j] = str[j];
-		j++;
-	}
-	if (str[i] == '\n')
-	{
-		line[j] = '\n';
-		j++;
-	}
-	line[j] = '\0';
-	return (line);
-}
-
-char	*get_next(char *str)
-{
-	int		i;
-	char	*new_line;
-
-	i = 0;
-	while (str[i] && str[i] != '\n')
-		i++;
-	if (str[i] == '\n')
-	{
-		new_line = ft_strdup(str + i + 1);
-		free(str);
-		return (new_line);
-	}
-	free(str);
-	return (NULL);
-}
-
-char	*ft_read(char *str, int fd)
-{
-	int		retread;
-	char	*buf;
-
-	retread = 1;
-	buf = malloc(BUFFER_SIZE + 1);
-	while (retread != 0 && !ft_strchr(str))
-	{
-		retread = read(fd, buf, BUFFER_SIZE);
-		if (retread < 0)
+		rd_bytes = read(fd, buff, BUFFER_SIZE);
+		if (rd_bytes == -1)
 		{
-			free(buf);
+			free(buff);
 			return (NULL);
 		}
-		buf[retread] = '\0';
-		str = ft_strjoin(str, buf);
+		buff[rd_bytes] = '\0';
+		left_str = ft_strjoin(left_str, buff);
 	}
-	free(buf);
-	return (str);
+	free(buff);
+	return (left_str);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*str;
 	char		*line;
-	int			retread;
+	static char	*left_str;
 
-	retread = 1;
-	if (fd < 0 || BUFFER_SIZE < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	left_str = ft_read_to_left_str(fd, left_str);
+	if (!left_str)
 		return (NULL);
-	str = ft_read(str, fd);
-	if (!str)
-		return (NULL);
-	if (str[0] == '\0')
-	{
-		free(str);
-		str = NULL;
-		return (NULL);
-	}
-	line = get_line(str);
-	str = get_next(str);
+	line = ft_get_line(left_str);
+	left_str = ft_new_left_str(left_str);
 	return (line);
-}
-
-int	main(void)
-{
-	int fd;
-	static char	*hold_buffer;
-	
-	fd = open("test.txt", O_RDONLY);
-	if (fd == -1)
-	{
-		printf("Error opening file");
-		return (1);
-	}
-	re
-	close(fd);
-	return (0);
 }
--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -1,15 +1,21 @@
-#ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_BONUS_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-# include <stdio.h>
 # include <stdlib.h>
-# include <unistd.h>
-# include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
 
 char	*get_next_line(int fd);
-int		ft_strchr(char *s);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strdup(char *src);
-int		len(char *src);
+char	*ft_read_to_left_str(int fd, char *left_str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *left_str, char *buff);
+size_t	ft_strlen(char *s);
+char	*ft_get_line(char *left_str);
+char	*ft_new_left_str(char *left_str);
 
 #endif
