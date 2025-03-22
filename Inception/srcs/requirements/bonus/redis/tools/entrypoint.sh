2174b24e519fe5f207e3a9bd67ea1796643d8287
Zero <hassan.winners@gmail.com>
2025-03-22 03:27:25 +0000

    Fix Def ports

--- Inception/srcs/requirements/bonus/redis/tools/entrypoint.sh
+++ Inception/srcs/requirements/bonus/redis/tools/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
+set -e  # Exit on error
 
-if [ ! -f "/etc/redis/redis.conf.bak" ]; then
-    cp /etc/redis/redis.conf /etc/redis/redis.conf.bak #We create the .bak to notify the program if it exists, don't go to the loop anymore
-    sed -i "s|bind 127.0.0.1|#bind 127.0.0.1|g" /etc/redis/redis.conf
-	
-    # sed -i "s|# requirepass foobared|requirepass $REDIS_PWD|g" /etc/redis.conf
-    sed -i "s|# maxmemory <bytes>|maxmemory 2mb|g" /etc/redis/redis.conf
-    sed -i "s|# maxmemory-policy noeviction|maxmemory-policy allkeys-lru|g" /etc/redis/redis.conf
+CONFIG_FILE="/etc/redis/redis.conf"
+
+# Modify only if changes are not already applied
+if ! grep -q "#bind 127.0.0.1" "$CONFIG_FILE"; then
+    echo "Configuring Redis..."
+    sed -i "s|bind 127.0.0.1|#bind 127.0.0.1|g" "$CONFIG_FILE"
+    sed -i "s|# maxmemory <bytes>|maxmemory 2mb|g" "$CONFIG_FILE"
+    sed -i "s|# maxmemory-policy noeviction|maxmemory-policy allkeys-lru|g" "$CONFIG_FILE"
 fi
 
-# Start Redis server in protected mode off
+# Start Redis server with protected mode disabled
 exec redis-server --protected-mode no
